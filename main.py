0fbc237889d2c67f501f5569ceb06558daa8b390
richhildebrand <richhildebrand@gmail.com>
2018-05-28 19:04:19 -0400

    use 30 columns

--- main.py
+++ main.py
@@ -23,25 +23,24 @@ id_column = 'Id'
 
 y, X, test_data = rph_get_X_y_and_test_data(train_file_path, test_file_path, column_to_predict)
 
-columns_to_encode = rph_get_columns_to_encode(X, 20)
+columns_to_encode = rph_get_columns_to_encode(X, 30)
 print(len(columns_to_encode))
 
 standard_columns = rph_get_standard_columns(X)
 columns_to_keep = columns_to_encode + standard_columns
 
 X = X[columns_to_keep]
+columns_to_drop = ['Fireplaces', 'GarageArea', 'MoSold', '1stFlrSF']
+X = rph_drop_columns(X, columns_to_drop)
 
 #rph_find_encoded_column_to_drop(y, X, test_data, columns_to_encode, id_column)
 #columns_to_drop = rph_find_non_object_column_to_drop(X, y, standard_columns)
 
-encoded_columns_to_drop = []
-#encoded_columns_to_drop = ['CentralAir']
-#encoded_columns_to_drop = ['CentralAir', 'GarageType']
+encoded_columns_to_drop = ['RoofStyle', 'FireplaceQu', 'Fence']
 for column in encoded_columns_to_drop: columns_to_encode.remove(column)
 
 
-columns_to_drop = ['Fireplaces', 'GarageArea', 'MoSold', '1stFlrSF'] + encoded_columns_to_drop
-X = rph_drop_columns(X, columns_to_drop)
+columns_to_drop = columns_to_drop + encoded_columns_to_drop
 test_data = rph_drop_columns(test_data, columns_to_drop)
 
 X, test_data = rph_encode_columns(X, test_data, columns_to_encode)
